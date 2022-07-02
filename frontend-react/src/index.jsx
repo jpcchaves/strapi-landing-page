b0cd573535e8b7e39312a612fff839bcf445826f
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-01 23:40:12 -0300

    introdução ao styled components

--- frontend-react/src/index.jsx
+++ frontend-react/src/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './templates/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GlobalStyles } from "./styles/global-styles";
+import Home from "./templates/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles />
   </React.StrictMode>
 );
